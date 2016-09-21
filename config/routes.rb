f72a9b72accbaf9c950a01fb23057fa815bf364a
Jeff McFadden <jeff@forgeapps.com>
2016-09-21 11:20:27 -0700

    Add sidekiq web.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
 
   devise_for :users
+
   
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+    
   resources :cameras
   resources :motion_events
   
