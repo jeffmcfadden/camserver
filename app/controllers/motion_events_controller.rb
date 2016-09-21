dbf0b9dc061c10c36453efd7cab76f5a06b1e645
Jeff McFadden <jeff@forgeapps.com>
2016-09-21 14:41:31 -0700

    Link to single camera events.

--- app/controllers/motion_events_controller.rb
+++ app/controllers/motion_events_controller.rb
@@ -3,7 +3,13 @@ class MotionEventsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @motion_events = MotionEvent.order( occurred_at: :desc ).page( params[:page] ).per( params[:per] )
+    @motion_events = MotionEvent.all
+    
+    if params[:camera_id].present?
+      @motion_events = @motion_events.where( camera_id: params[:camera_id] )
+    end
+    
+    @motion_events = @motion_events.order( occurred_at: :desc ).page( params[:page] ).per( params[:per] )
   end
   
   def show
