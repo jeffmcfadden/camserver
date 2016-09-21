4e12f3fada214fb597aaa072203323d84136c4b1
Jeff McFadden <jeff@forgeapps.com>
2016-09-21 12:37:58 -0700

    Trying time zone again.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception, prepend: true
+  
+  before_action :set_time_zone
+  
+  def set_time_zone
+    Time.zone = "US/Arizona"
+  end
+  
 end
