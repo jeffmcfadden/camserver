69c3bd2a616631280850638592a0cb507d6606f5
Jeff McFadden <jeff@mcfadden.io>
2019-07-16 16:34:14 -0700

    Add some sidekiq checking to the cameras list. See if that helps.

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -3,4 +3,11 @@ class Camera < ApplicationRecord
 
   enum camera_type: [ :foscam_8910, :foscam_9821, :amcrest_ipm721, :foscam_9821_ftp ]  
 
+  def sidekiq_worker_running?
+    scheduled = Sidekiq::ScheduledSet.new.select {|j| j.klass == 'CloneCameraCardWorker' && j.args == [self.id] }.first.present?
+    running   = Sidekiq::Queue.new("low").select{ |j| j.klass == 'CloneCameraCardWorker' && j.args == [self.id] }.first.present?
+    
+    scheduled || running
+  end
+
 end
