10e1e6900f8bcbb21fa634028f6a747de94f9fcd
Jeff McFadden <jeff@mcfadden.io>
2019-10-10 16:14:43 -0700

    Switch to disk only.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.active_storage.service = :production
+  config.active_storage.service = :disk
     
   # config.force_ssl = true
 end
